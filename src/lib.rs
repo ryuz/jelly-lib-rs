b0fe19474fa689f969db39dfa9e764306eb144d3
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-10-12 10:24:25 +0000

    Add video DMA driver module and implement portable delay functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,26 @@ extern crate std;
 
 pub mod i2c_access;
 pub mod imx219_control;
+pub mod video_dma_driver;
 
 #[cfg(feature = "std")]
 pub mod linux_i2c;
+
+
+
+#[cfg(feature = "std")]
+pub fn portable_delay(duration : core::time::Duration) {
+    std::thread::sleep(duration);
+}
+
+#[cfg(not(feature = "std"))] 
+pub fn portable_delay(duration: core::time::Duration) {
+    let us = duration.as_micros() as u64;
+    let iterations = us * 1000; // 概算
+    for _ in 0..iterations {
+        unsafe {
+            core::arch::asm!("nop");
+        }
+    }
+}
+
